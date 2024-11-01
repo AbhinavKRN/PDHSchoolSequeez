9b5ec2cd8e6c13d446e2984210c9fc2be4fc12c3
Utkarsh <78083867+hsraktu17@users.noreply.github.com>
2024-11-01 17:33:26 +0530

    button action added

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -1,8 +1,11 @@
 "use client";
 import { motion } from "framer-motion";
 import { HeroHighlight, Highlight } from "./ui/hero-highlight";
+import { useRouter } from "next/navigation";
 
 export function HeroHighlightDemo() {
+  const router = useRouter();
+
   return (
     <HeroHighlight>
       <motion.h1
@@ -24,12 +27,39 @@ export function HeroHighlightDemo() {
           Elevate Your Career at PDH School of Tech and Business!
         </div>
         <div className="font-normal">
-          Learn from industry leaders and innovate in pur incubator programs.{" "}
+          Learn from industry leaders and innovate in pre incubator programs.{" "}
         </div>
         <Highlight className="text-black dark:text-white">
           Unleash Your Potential, Join Today!
         </Highlight>
       </motion.h1>
+      <motion.div
+        initial={{
+          opacity: 0,
+          y: 20,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 0.6,
+          ease: [0.4, 0.0, 0.2, 1],
+          delay: 0.3,
+        }}
+        className="mt-8 flex justify-center"
+      >
+        <button
+          onClick={() =>
+            router.push(
+              "https://docs.google.com/forms/d/e/1FAIpQLSehQlbNermYzdRPcAGe3I2ujBUIIB8Qmw7-VsHEhRHnk7YzWw/viewform",
+            )
+          }
+          className="px-6 py-3 bg-white text-black  rounded-md text-lg font-semibold hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+        >
+          Join Today!
+        </button>
+      </motion.div>
     </HeroHighlight>
   );
 }
